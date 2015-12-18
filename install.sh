0ebe9c35b47eda8d750678a9fca076848389a26b
Cole Brokamp <cole.brokamp@gmail.com>
2015-12-18 12:28:53 -0500

    fixed install script and renamed it

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ olddir=~/dotfiles_old
 files=`ls | grep -v "makeSymLinks.sh" | grep -v "README.md" | grep -v "CB.terminal"`  
 
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
+echo "Creating $olddir for backup of any existing dotfiles"
 mkdir -p $olddir
 
 cd $dir
@@ -13,9 +13,12 @@ cd $dir
 # move any existing dotfiles in homedir to dotfiles_old directory
 # then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    mv ~/.$file ~/dotfiles_old/
+    if [ -f ~/.$file ]; then
+        mv -f ~/.$file $olddir 2>/dev/null
+        echo "Backing up $file to $olddir"
+    fi
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/$file ~/.$file 2>/dev/null
 done
 
 # remove the extra created nano folder
