0b6c56ac406655dd776a87c5fdbb1f9d880ec029
Cole Brokamp <cole.brokamp@gmail.com>
2017-01-23 16:09:38 -0500

    don't need ubuntu gis to install geos and gdal libraries

--- install/install_geos_gdal_proj4_for_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update && sudo apt-get install -y \
-    software-properties-common python-software-properties
-
-# make sure to have software properties installed as
-RUN add-apt-repository -y ppa:ubuntugis/ppa         
-RUN apt-get update && apt-get install -y gdal-bin
