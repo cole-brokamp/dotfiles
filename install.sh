38651b2565381926312cf6a78048b1a52c3b017d
Cole Brokamp <cole.brokamp@gmail.com>
2017-11-09 16:57:38 -0500

    source bash_profile from bashrc

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cd $dir
 echo -e "\nCreating symlinks"
 echo "=============================="
 
-linkables=( "bash_profile" "gitconfig" "gitignore" "hushlogin" "nanorc" "screenrc" "tmux.conf" "vimrc" "Rprofile" "inputrc")
+linkables=( "bash_profile" "bashrc" "gitconfig" "gitignore" "hushlogin" "nanorc" "screenrc" "tmux.conf" "vimrc" "Rprofile" "inputrc")
 
 for file in "${linkables[@]}" ; do
     echo "Creating symlink for $file"
