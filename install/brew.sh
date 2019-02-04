ced6064d7d6ef6229f955f868f25baef42c7a302
Cole Brokamp <cole.brokamp@gmail.com>
2019-02-04 15:38:35 -0500

    brew to 2.0!

--- install/brew.sh
+++ install/brew.sh
@@ -2,9 +2,7 @@
 
 if test ! $(which brew); then
     echo "Installing homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
-    # temporarily set path so brew is usable on linux
-    PATH="$HOME/.linuxbrew/bin:$PATH"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
