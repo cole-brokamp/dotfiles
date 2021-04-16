451bab24caea0c9283ecaec1c389dab72b07ed76
Cole Brokamp <cole.brokamp@gmail.com>
2021-04-16 16:35:38 -0400

    maintain lintr settings

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cd $dir
 echo -e "\nCreating symlinks"
 echo "=============================="
 
-linkables=( "bash_profile" "bashrc" "gitconfig" "gitignore" "hushlogin" "screenrc" "tmux.conf" "vimrc" "Rprofile" "inputrc" "emacs")
+linkables=( "bash_profile" "bashrc" "gitconfig" "gitignore" "hushlogin" "screenrc" "tmux.conf" "vimrc" "Rprofile" "lintr" "inputrc" "emacs")
 
 for file in "${linkables[@]}" ; do
     echo "Creating symlink for $file"
