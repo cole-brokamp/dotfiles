4050dc2909ac2064f5d73b346c0b1618542c1ae1
Cole Brokamp <cole.brokamp@gmail.com>
2023-02-13 15:25:32 -0500

    fix low res pdfs on macOS

--- colemacs/colemacs-pdf.el
+++ colemacs/colemacs-pdf.el
@@ -1,8 +1,11 @@
-(use-package doc-view)
+(use-package doc-view
+  :custom
+  (doc-view-resolution '300)
+)
 
 
-(cole/local-leader-keys doc-view-mode-map
+;; (cole/local-leader-keys doc-view-mode-map
 ;; doc-view-set-slice-using-mouse
 ;; doc-view-reset-slice
 ;; doc-view-search
-  )
+  ;; )
