8b2ec35e88ed9ad62bb102d8efd8ced5b87ac1ae
Cole Brokamp <cole-brokamp@users.noreply.github.com>
2023-08-06 10:48:27 -0400

    Update macOS.sh

--- install/macOS.sh
+++ install/macOS.sh
@@ -1,48 +1,31 @@
 #!/bin/bash
 
-# echo "Display full POSIX path as Finder window title"
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true 
-
 # Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
 # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true 
 defaults write com.apple.finder QuitMenuItem -bool true
-
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder AppleShowAllFiles 1
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
-
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
-# Prevent Time Machine from prompting to use new hard drives as backup volume
+# Time Machine
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-
-# Disable local Time Machine backups
 hash tmutil &> /dev/null && sudo tmutil disablelocal
 
-# Show the main window when launching Activity Monitor
+# Activity Monitor
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-
-# Visualize CPU usage in the Activity Monitor Dock icon
 defaults write com.apple.ActivityMonitor IconType -int 5
-
-# Show all processes in Activity Monitor
 defaults write com.apple.ActivityMonitor ShowCategory -int 0
-
-# Sort Activity Monitor results by CPU usage
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
-
-###############################################################################
-# Kill affected applications                                                  #
-###############################################################################
 
 for app in "Activity Monitor" "cfprefsd" "Dock" "Finder" "SystemUIServer" "Terminal"; do
   killall "${app}" &> /dev/null
