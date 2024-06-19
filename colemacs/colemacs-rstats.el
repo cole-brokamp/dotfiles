aef84d39724b790d00f8f4c079033d82c735873f
Cole Brokamp <cole.brokamp@gmail.com>
2024-06-19 10:21:18 -0400

    more rstats hydras

--- colemacs/colemacs-rstats.el
+++ colemacs/colemacs-rstats.el
@@ -39,22 +39,9 @@
   "o" '(cole/ess-eval-word :which-key "print object")
   "O" '(cole/ess-glimpse-word :which-key "glimpse object")
   "g" '(cole/r-graphics/body :which-key "graphics")
-  ;; "gg" '(cole/ess-graphics-view :which-key "open graphics server")
-  ;; "gs" '(cole/ess-graphics-start :which-key "start graphics server")
-  ;; "gx" '(cole/ess-graphics-stop :which-key "stop graphics server")
   "=" '(format-all-buffer t :which-key "format buffer")
-  "t" '(:ignore t :which-key "test")
-  "tt" '((lambda () (interactive) (ess-eval-linewise "devtools::test()")) :which-key "test")
-  "tf" '(cole/ess-devtools-test-file :which-key "test file")
-  "tc" '((lambda () (interactive) (ess-eval-linewise "devtools::test_coverage()")) :which-key "test coverage")
-  "ts" '((lambda () (interactive) (ess-eval-linewise "devtools::snapshot_review()")) :which-key "snapshot review")
-  "d" '(:ignore t :which-key "devtools")
-  "dl" '((lambda () (interactive) (ess-eval-linewise "devtools::load_all()")) :which-key "load all")
-  "dl" '((lambda () (interactive) (ess-eval-linewise "devtools::document()")) :which-key "document")
-  "dc" '((lambda () (interactive) (ess-eval-linewise "devtools::check()")) :which-key "check")
-  "dr" '((lambda () (interactive) (ess-eval-linewise "devtools::build_readme()")) :which-key "build readme from Rmd")
-  "ds" '((lambda () (interactive) (ess-eval-linewise "devtools::build_site()")) :which-key "build pkgdown site")
-  "dm" '((lambda () (interactive) (ess-eval-linewise "devtools::build_manual()")) :which-key "build pdf manual")
+  "t" '(cole/r-test/body :which-key "test")
+  "d" '(cole/r-devtools/body :which-key "devtools")
   "h" '(lsp-ui-doc-glance :which-key "help")
   "H" '(lsp-describe-thing-at-point :which-key "help in new window")
   "w" '(cole/ess-web-help :which-key "help in browser")
@@ -66,35 +53,36 @@
   "sq" '(ess-quit :which-key "quit")
   )
 
+(defhydra cole/r-devtools (:exit t)
+  "devtools package actions"
+  ("l" (lambda () (interactive) (ess-eval-linewise "devtools::load_all()")) "load all")
+  ("d" (lambda () (interactive) (ess-eval-linewise "devtools::document()")) "document")
+  ("c" (lambda () (interactive) (ess-eval-linewise "devtools::check()")) "check")
+  ("r" (lambda () (interactive) (ess-eval-linewise "devtools::build_readme()")) "build readme from Rmd")
+  ("s" (lambda () (interactive) (ess-eval-linewise "devtools::build_site()")) "build pkgdown site")
+  ("m" (lambda () (interactive) (ess-eval-linewise "devtools::build_manual()")) "build pdf manual")
+)
+
+(defhydra cole/r-test (:exit t)
+  "test with devtools"
+  ("t" (lambda () (interactive) (ess-eval-linewise "devtools::test()")) "test package")
+  ("c" (lambda () (interactive) (ess-eval-linewise "devtools::test_coverage()")) "coverage")
+  ("s" (lambda () (interactive) (ess-eval-linewise "devtools::snapshot_review()")) "snapshot review")
+  ("f" cole/ess-devtools-test-file "file")
+)
+
 (defhydra cole/r-graphics (:exit t)
   "graphics httpgd server"
   ("g" (lambda () (interactive) (ess-eval-linewise "if (!names(dev.cur()) == 'unigd') httpgd::hgd(); httpgd::hgd_browse()")) "open")
   ("s" (lambda () (interactive) (ess-eval-linewise "httpgd::hgd()")) "start")
   ("x" (lambda () (interactive) (ess-eval-linewise "httpgd::hgd_close()")) "close")
   ("q" nil "quit")
-  )
+)
   
-
-;; (defun cole/ess-devtools-load-all ()
-;;   (interactive)
-;;   (ess-eval-linewise "devtools::load_all()"))
-
 ; TODO how to get file name of active buffer?
 (defun cole/ess-devtools-test-file ()
   (interactive)
   (ess-eval-linewise "testthat::test_file()"))
-
-(defun cole/ess-graphics-stop ()
-  (interactive)
-  (ess-eval-linewise "httpgd::hgd_close()"))
-
-(defun cole/ess-graphics-start ()
-  (interactive)
-  (ess-eval-linewise "httpgd::hgd()"))
-
-(defun cole/ess-graphics-view ()
-  (interactive)
-  (ess-eval-linewise "if (!names(dev.cur()) == 'httpgd') httpgd::hgd(silent = TRUE); httpgd::hgd_browse()"))
 
 (defun cole/insert-pipe ()
   "Insert a |>"
